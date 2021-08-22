74cf70fe24f4a05b156ca052cf5a99f60d6b1bd8
52atiq <atiqit52@gmail.com>
2021-08-22 08:51:22 +0600

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,28 @@
+
+const data = {
+    products:[
+        {id: '1',
+         name:'Mackbook',
+         price:1400,
+         Image:'https://cdn.pixabay.com/photo/2014/05/02/21/50/laptop-336378_960_720.jpg ',
+        
+        },
+
+        {id: '1',
+        name:'Old car',
+        price:1700,
+        Image:' https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg',
+       
+       },
+
+       {id: '1',
+       name:'Shoes',
+       price:2300,
+       Image:'https://cdn.pixabay.com/photo/2016/01/19/18/06/brown-shoes-1150071_960_720.jpg ',
+      
+      },
+    ]
+
+}
+
+export default data;
